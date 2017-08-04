30dcd8be02ef982a5fc08fe0615f6379f1c8d3ac
Vicente Jesús Mayor Gallego <vmayor93@gmail.com>
2017-08-04 15:41:47 +0200

    Parseo básico implementado en Ficha de película

--- app/src/main/java/molina/alfonso/hype/Ficha.java
+++ app/src/main/java/molina/alfonso/hype/Ficha.java
@@ -2,7 +2,9 @@ package molina.alfonso.hype;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -10,6 +12,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -76,10 +87,89 @@ public class Ficha extends Fragment {
 
     //TODO: ¡PREPARAR CONTENIDO DEL FRAGMENT!
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         ((TextView) view.findViewById(R.id.ficha_titulo)).setText(titulo);
+
+        AsyncTask task = new AsyncTask() {
+
+            String sinopsis;
+            String director;
+            String imagen_url;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected Void doInBackground(Object[] objects) {
+
+                try {
+                    String contenido = getHTML(link);
+                    // Aquí el parseo
+
+                    // Saco la sinopsis
+                    Pattern pattern = Pattern.compile("<dt>Sinopsis</dt>(.*?)<dd itemprop=\"description\">(.*?)</dd>");
+                    Matcher matcher = pattern.matcher(contenido);
+
+                    while (matcher.find()) {
+                        sinopsis = matcher.group(2);
+                        Log.d(TAG, matcher.group(2));
+                    }
+
+                    pattern = Pattern.compile("<dt id=\"full-director\">Director</dt>(.*?)<span itemprop=\"name\">(.*?)</span>");
+                    matcher = pattern.matcher(contenido);
+
+                    while (matcher.find()) {
+                        director = matcher.group(2);
+                        Log.d(TAG, matcher.group(2));
+                    }
+
+                    pattern = Pattern.compile("<a id=\"main-poster\" href=\"#\">(.*?)<img itemprop=\"image\" src=\"(.*?)\"");
+                    matcher = pattern.matcher(contenido);
+
+                    while (matcher.find()) {
+                        imagen_url = matcher.group(2);
+                        Log.d(TAG, matcher.group(2));
+                    }
+
+                }catch (Exception e){
+                    Log.e(TAG, e.toString());
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                super.onPostExecute(o);
+                ((TextView) view.findViewById(R.id.ficha_sinopsis)).setText(sinopsis);
+            }
+
+            @NonNull
+            private String getHTML(String url) throws IOException {
+                Log.d(TAG, "Obteniendo contenido HTML desde " + url);
+                // Build and set timeout values for the request.
+                URLConnection connection = (new URL(url)).openConnection();
+                connection.setConnectTimeout(5000);
+                connection.setReadTimeout(5000);
+                connection.connect();
+
+                // Read and store the result line by line then return the entire string.
+                InputStream in = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuilder html = new StringBuilder();
+                for (String line; (line = reader.readLine()) != null; ) {
+                    html.append(line);
+                }
+                in.close();
+
+                return html.toString();
+            }
+        };
+
+        task.execute();
     }
 
     // TODO: Rename method, update argument and hook method into UI event
